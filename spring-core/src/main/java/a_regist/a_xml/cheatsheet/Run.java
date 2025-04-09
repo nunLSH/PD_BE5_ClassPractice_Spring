45c157e2d4dd1520d223c884c92c3d1d470624a1
imseohyeon <forestlight749@gmail.com>
2025-04-09 10:24:52 +0900

    (0409) feat: Spring XML 기반 CheatSheet DI 설정 예제 추가

---
+++ spring-core/src/main/java/a_regist/a_xml/cheatsheet/Run.java
@@ -0,0 +1,19 @@
+package a_regist.a_xml.cheatsheet;
+
+import java.time.LocalDate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Run {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+            "/regist/xml/cheatsheet/application-context.xml"
+        );
+
+        LocalDate now = context.getBean("now", LocalDate.class);
+        System.out.println(now);
+
+    }
+
+}
