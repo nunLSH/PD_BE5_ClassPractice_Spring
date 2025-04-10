834e8e5832359969c2e23196ea272c06a0264ddd
imseohyeon <forestlight749@gmail.com>
2025-04-10 13:14:27 +0900

    (0410) feat: 유효성 검사 및 오류 메시지 추가

--- spring-mvc/src/main/java/com/grepp/spring/app/controller/web/urlencoded/UrlEncodedController.java
+++ spring-mvc/src/main/java/com/grepp/spring/app/controller/web/urlencoded/UrlEncodedController.java
@@ -1,15 +1,18 @@
-package com.grepp.spring.app.controller.web.urlencoded.validator;
+package com.grepp.spring.app.controller.web.urlencoded;
 
 import com.grepp.spring.app.controller.web.urlencoded.form.UrlEncodedForm;
 import com.grepp.spring.app.model.urlencoded.dto.UrlEncodedDto;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import java.time.LocalDateTime;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,13 +22,13 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 // NOTE 01 @Controller
-// 1. 해당 클래스를 bean 으로 등록
-// 2. @RequestMapping : 요청 URL 과 Controller method 를 매핑
-// 3. @GetMapping, @PostMapping : 지정된 Http Method 와 URL 을 Controller 의 method 와 매핑
-// 4. @RequestPara : format 이 x-www-form-urlEncoded 인 요청파라미터를
-//                   Controller 의 매개변수에 매핑
+// 1. 해당 클래스를 bean으로 등록
+// 2. @RequestMapping : 요청 URL과 Controller method 를 매핑
+// 3. @GetMapping, @PostMapping... : 지정된 Http Method 와 URL 을 Controller 의 method 와 매핑
+// 4. @RequestParam : format 이 x-www-form-urlEncoded 인 요청파라미터를
+//                    Controller 의 매개변수에 매핑
 // 5. @ModelAttribute : 요청파라미터를 메서드의 매개변수에 선언한 Form 객체에 매핑
-//                      Model 객체의 attribute 에 Form 객체를 자동으로 추가
+//                    Model 객체의 attribute 에 Form 객체를 자동으로 추가
 @Controller
 @RequestMapping("form")
 @Slf4j
@@ -38,12 +41,11 @@ public class UrlEncodedController {
     // 1. String : view 경로
     // 2. void   : 요청 url 을 view 경로로 지정
     // 3. ModelAndView :
-    //           model : Controller 애서 view 로 전달할 데이터를 저장하는 객체
-    //           view  : view 의 경로
-
+    //        model : Controller 에서 view 로 전달할 데이터를 저장하는 객체
+    //        view  : view 의 경로
     @GetMapping
-    public String form() {
-        log.info("form 메서드");
+    public String form(UrlEncodedForm form){
+        log.debug("form  메서드");
         // forward
         return "spring/form";
     }
@@ -51,21 +53,30 @@ public class UrlEncodedController {
     @PostMapping
     public String form(
         // 암묵적인 @ModelAttribute
+        @Valid
         UrlEncodedForm form,
-        Model model) {
+        BindingResult bindingResult,
+        Model model){
+
         log.info("model : {}", model);
-        log.info("form : {}", model);
+        log.info("form : {}" , form);
+        log.info("bindingResult : {}" , bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "spring/form";
+        }
 
         UrlEncodedDto dto = new UrlEncodedDto(
             form.getUserId(),
             form.getEmail(),
             form.getTel());
+
         model.addAttribute("dto", dto);
         return "spring/result";
     }
 
     @PostMapping("modelandview")
-    public ModelAndView modelandview(UrlEncodedForm form) {
+    public ModelAndView modelAndView(UrlEncodedForm form){
         ModelAndView mav = new ModelAndView();
 
         UrlEncodedDto dto = new UrlEncodedDto(
@@ -83,13 +94,13 @@ public class UrlEncodedController {
     public String pathVariable(
         @PathVariable()
         String id,
-        @RequestParam(required = false)
+        @RequestParam
         String email,
         String tel,
         LocalDateTime createdAt,
         Model model
-    ) {
-        log.info("createdAt : {}", createdAt);
+    ){
+        log.info("createdAt : {} ",  createdAt);
         model.addAttribute("dto",
             new UrlEncodedDto(id, email, tel));
         return "spring/result";
@@ -114,9 +125,7 @@ public class UrlEncodedController {
     }
 
     @GetMapping("cookie/regist")
-    public String registCookie(
-        HttpServletResponse response
-    ){
+    public String registCookie(HttpServletResponse response){
         Cookie cookie = new Cookie("server_cookie", "this_is_server_cookie");
         response.addCookie(cookie);
         return "spring/cookie";
--- spring-mvc/src/main/java/com/grepp/spring/app/controller/web/urlencoded/form/UrlEncodedForm.java
+++ spring-mvc/src/main/java/com/grepp/spring/app/controller/web/urlencoded/form/UrlEncodedForm.java
@@ -1,11 +1,14 @@
 package com.grepp.spring.app.controller.web.urlencoded.form;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 
 @AllArgsConstructor
 @Getter
@@ -13,9 +16,15 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 public class UrlEncodedForm {
+    @NotBlank
     private String userId;
-    private String password;
-    private String email;
-    private String tel;
 
+    @Length(min = 4, max = 20)
+    private String password;
+
+    @Email(message = "이메일 양식이 아닙니다.")
+    private String email;
+
+    @Length(min = 9, max = 11, message = "전화번호는 9~11자리 입니다.")
+    private String tel;
 }
