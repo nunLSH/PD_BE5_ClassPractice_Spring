d746b825d4c3e0289afa27c5d76be2f55949e352
imseohyeon <forestlight749@gmail.com>
2025-04-16 00:00:40 +0900

    (0415) feat: 스크롤 및 더블클릭 시 target 요소 표시 기능 추가

--- spring-mybatis/src/main/webapp/assets/js/book-list.js
+++ spring-mybatis/src/main/webapp/assets/js/book-list.js
@@ -1,9 +1,10 @@
 let page = 1;
 const size = 3;
-const observeTarget = document.querySelector(".page-footer");
+const observeTarget = document.querySelector(".target");
 
 const createBook = data => {
-  const clone = document.querySelector('#bookListTemplate').firstElementChild.cloneNode(true);
+  const clone = document.querySelector(
+      '#bookListTemplate').firstElementChild.cloneNode(true);
   const thumbnail = clone.querySelector('.thumbnail');
   const title = clone.querySelector('.title');
   const author = clone.querySelector('.author');
@@ -14,14 +15,23 @@ const createBook = data => {
   return clone;
 }
 
+document.addEventListener('dblclick', ev => {
+  observeTarget.style.display = 'block';
+});
+
+document.addEventListener('scroll', ev => {
+  observeTarget.style.display = 'block';
+});
+
 const intersectionObserver = new IntersectionObserver(
     async function (entries) {
 
-      const response = await fetch(`http://localhost:8080/api/book/list?page=${++page}&size=${size}`);
+      const response = await fetch(
+          `http://localhost:8080/api/book/list?page=${++page}&size=${size}`);
       const data = await response.json();
       const books = data.data.bookInfos;
 
-      if(!books || books.length === 0){
+      if (!books || books.length === 0) {
         intersectionObserver.unobserve(observeTarget);
         return;
       }
@@ -30,6 +40,7 @@ const intersectionObserver = new IntersectionObserver(
         const clone = createBook(e);
         document.querySelector('.book-list').appendChild(clone);
       })
+      observeTarget.style.display = 'none';
     });
 
 intersectionObserver.observe(observeTarget);
