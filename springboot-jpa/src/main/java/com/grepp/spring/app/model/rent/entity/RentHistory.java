6e9dcd6d74df4e44b86af7140197da6b79541f77
imseohyeon <forestlight749@gmail.com>
2025-04-18 16:19:25 +0900

    (0418) refactor: JPA Entity로 전환 & BaseEntity 상속 적용 & 디렉토리 변경

---
+++ springboot-jpa/src/main/java/com/grepp/spring/app/model/rent/entity/RentHistory.java
@@ -0,0 +1,35 @@
+package com.grepp.spring.app.model.rent.entity;
+
+import com.fasterxml.jackson.databind.ser.Serializers.Base;
+import com.grepp.spring.app.model.rent.code.RentState;
+import com.grepp.spring.infra.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class RentHistory extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Rent rent;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private RentBook rentBook;
+    private Long bookId;
+    @Enumerated(EnumType.STRING)
+    private RentState state;
+}
