df057a073b879c9683fb92f7cf0df77940725d93
imseohyeon <forestlight749@gmail.com>
2025-04-22 11:45:17 +0900

    (0422) feat: 버스정류장 API 연동 및 초기 데이터 저장 로직 구현

---
+++ springboot-boilerplate/src/test/java/com/grepp/spring/infra/feign/client/busstop/BusStopApiTest.java
@@ -0,0 +1,23 @@
+package com.grepp.spring.infra.feign.client.busstop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusStopApiTest {
+
+    @Autowired
+    BusStopApi busStopApi;
+
+    @Value("bus-stop.apikey")
+    String apiKey;
+
+    @Test
+    public void feignTest(){
+        System.out.println(busStopApi.getBusStop("6274784e696c6563373162656f584d",
+            1,5));
+    }
+
+}
