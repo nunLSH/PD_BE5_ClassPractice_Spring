ebde0740a0207295ecae7fd59cff2de1c16703f4
imseohyeon <forestlight749@gmail.com>
2025-04-22 11:42:55 +0900

    (0422) feat: 서울시 버스정류장 위치 조회용 Feign 클라이언트 추가

---
+++ springboot-boilerplate/src/main/java/com/grepp/spring/infra/feign/client/busstop/BusStopApi.java
@@ -0,0 +1,20 @@
+package com.grepp.spring.infra.feign.client.busstop;
+
+import com.grepp.spring.infra.feign.client.busstop.dto.BusStopResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "busStopApi", url = "http://openapi.seoul.go.kr:8088/")
+public interface BusStopApi {
+
+    @GetMapping("{apiKey}/json/busStopLocationXyInfo/{startIndex}/{endIndex}")
+    BusStopResponse getBusStop(
+        @PathVariable
+        String apiKey,
+        @PathVariable
+        int startIndex,
+        @PathVariable
+        int endIndex
+    );
+}
