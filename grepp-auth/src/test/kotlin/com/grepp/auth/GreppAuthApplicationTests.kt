f70b74aacf6a25cdebe5a8bb52d671af3742085e
imseohyeon <forestlight749@gmail.com>
2025-06-03 17:12:41 +0900

    (0603) feat: 자동생성된 테스트 코드

---
+++ grepp-auth/src/test/kotlin/com/grepp/auth/GreppAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.grepp.auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GreppAuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
