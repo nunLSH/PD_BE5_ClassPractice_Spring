afe5f32a4d84690bb7138beab1201af4e5a616f4
imseohyeon <forestlight749@gmail.com>
2025-06-03 13:03:12 +0900

    chore (0603): gradle 일부 수정

--- grepp-auth/build.gradle.kts
+++ grepp-auth/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
 
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
