9ef08e34ffa95920bdfd97ad190962d6a4b059c9
imseohyeon <forestlight749@gmail.com>
2025-06-04 14:18:17 +0900

    (0604) feat:(auth-server) Spring Authorization Server 설정 클래스 추가

---
+++ grepp-auth/src/main/kotlin/com/grepp/auth/infra/config/security/AuthorizationServerSecurityFilterChain.kt
@@ -0,0 +1,172 @@
+package com.grepp.auth.infra.config.security
+
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.http.MediaType
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
+
+
+@Configuration
+@EnableWebSecurity
+class AuthorizationServerSecurityFilterChain {
+    @Bean
+    @Order(1)
+    @Throws(Exception::class)
+    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        val authorizationServerConfigurer =
+            OAuth2AuthorizationServerConfigurer.authorizationServer()
+
+        http
+            .cors{it.configurationSource(corsConfigurationSource())}
+            .securityMatcher(authorizationServerConfigurer.endpointsMatcher)
+            .with<OAuth2AuthorizationServerConfigurer>(
+                authorizationServerConfigurer
+            ) { authorizationServer: OAuth2AuthorizationServerConfigurer ->
+                authorizationServer
+                    .oidc(Customizer.withDefaults())
+            } // Enable OpenID Connect 1.0
+
+            .authorizeHttpRequests(
+                Customizer { it.anyRequest().authenticated() }
+            )
+            .exceptionHandling {
+                it.defaultAuthenticationEntryPointFor(
+                    LoginUrlAuthenticationEntryPoint("/login"),
+                    MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                )
+            }
+
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    @Throws(Exception::class)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors{it.configurationSource(corsConfigurationSource())}
+            .authorizeHttpRequests(
+                Customizer {
+                    it.anyRequest().authenticated()
+                }
+            )
+            .formLogin(){it.defaultSuccessUrl("http://localhost:8080")}
+
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val userDetails: UserDetails = User.withDefaultPasswordEncoder()
+            .username("test")
+            .password("123qwe!@#")
+            .roles("USER")
+            .build()
+
+        return InMemoryUserDetailsManager(userDetails)
+    }
+
+    @Bean
+    fun registeredClientRepository(): RegisteredClientRepository {
+        val grepp = RegisteredClient.withId(UUID.randomUUID().toString())
+            .clientId("grepp")
+            .clientSecret("{noop}secret")
+            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+            .redirectUri("http://localhost:8080/login/oauth2/code/grepp")
+            .postLogoutRedirectUri("http://localhost:8080/")
+            .scope(OidcScopes.OPENID)
+            .scope(OidcScopes.PROFILE)
+            .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+            .build()
+        return InMemoryRegisteredClientRepository(grepp)
+    }
+
+    @Bean
+    fun jwkSource(): JWKSource<SecurityContext> {
+        val keyPair: KeyPair = generateRsaKey()
+        val publicKey = keyPair.public as RSAPublicKey
+        val privateKey = keyPair.private as RSAPrivateKey
+        val rsaKey: RSAKey = RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build()
+        val jwkSet: JWKSet = JWKSet(rsaKey)
+        return ImmutableJWKSet<SecurityContext>(jwkSet)
+    }
+
+    @Bean
+    fun jwtDecoder(jwkSource: JWKSource<SecurityContext?>?): JwtDecoder {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
+    }
+
+    @Bean
+    fun authorizationServerSettings(): AuthorizationServerSettings {
+        return AuthorizationServerSettings.builder().build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfig = CorsConfiguration()
+        corsConfig.setAllowedOriginPatterns(
+            listOf(
+                "http://localhost:8080"
+            )
+        )
+        corsConfig.allowedMethods = mutableListOf("GET", "POST")
+        corsConfig.allowedHeaders = listOf("*")
+        corsConfig.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+        return source
+    }
+
+    companion object {
+        private fun generateRsaKey(): KeyPair {
+            val keyPair: KeyPair
+            try {
+                val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+                keyPairGenerator.initialize(2048)
+                keyPair = keyPairGenerator.generateKeyPair()
+            } catch (ex: Exception) {
+                throw IllegalStateException(ex)
+            }
+            return keyPair
+        }
+    }
+}
