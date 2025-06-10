355084fa774516c1237f3770ba7200bd2aa505f6
imseohyeon <forestlight749@gmail.com>
2025-06-10 19:15:43 +0900

    (0610) refactor: App 클래스를 GreppAuthApplication으로 이름 변경

--- grepp-auth/src/main/kotlin/com/grepp/auth/App.kt
+++ grepp-auth/src/main/kotlin/com/grepp/auth/App.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class App
+class GreppAuthApplication
 
 fun main(args: Array<String>) {
-    runApplication<App>(*args)
+    runApplication<GreppAuthApplication>(*args)
 }
