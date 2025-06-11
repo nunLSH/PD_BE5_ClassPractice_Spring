58a21d6671f6d2ce5ccdb73821861d062279bb93
imseohyeon <forestlight749@gmail.com>
2025-06-11 12:09:25 +0900

    (0611) chore: 불필요한 import 정리

--- chat-service-A/src/main/kotlin/com/grepp/spring/app/controller/websocket/team/TeamChatController.kt
+++ chat-service-A/src/main/kotlin/com/grepp/spring/app/controller/websocket/team/TeamChatController.kt
@@ -1,10 +1,8 @@
 package com.grepp.spring.app.controller.websocket.team
 
 import com.grepp.spring.app.controller.websocket.team.payload.ChatMessage
-import com.grepp.spring.infra.response.WebSocketResponse
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
