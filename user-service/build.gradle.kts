444256ddc890a2d1002d7e65ff79cd544c8946e8
imseohyeon <forestlight749@gmail.com>
2025-06-11 09:04:13 +0900

    (0610) chore: Redis 관련 의존성 추가

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.modelmapper:modelmapper:3.2.2")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
